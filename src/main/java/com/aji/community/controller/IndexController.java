4127f782fe2841801e235d570d0926775ff784a8
JiA1996 <aji0726@outlook.com>
2019-06-07 17:13:43 +0800

    added Bootstrap

--- src/main/java/com/aji/community/controller/IndexController.java
+++ src/main/java/com/aji/community/controller/IndexController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class helloController {
+public class IndexController {
 
     @GetMapping("/hello")
-    public String print_hello(@RequestParam(name = "usrName") String name, Model model){
-        model.addAttribute("name", name);
-        return "hello";
+    public String print_hello(){
+        return "index";
     }
 }
